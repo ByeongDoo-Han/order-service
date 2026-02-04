76955458fbc2166ca1a58e45c3e92ab9c9171032
byeongdoohan <quden04@gmail.com>
2026-02-04 21:55:31 +0900

    init project

---
+++ src/test/kotlin/com/example/sajeon/SajeonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sajeon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SajeonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
